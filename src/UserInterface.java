8ea194359144f034aa1b8a0f873bc2985d282e51
sidanan <134073838+sidanan@users.noreply.github.com>
2023-06-26 16:49:31 -0400

    Update UserInterface.java

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,22 +1,42 @@
 import java.util.Scanner;
 
 public class UserInterface {
-    private static Scanner in = new Scanner(System.in);
-    public static void main(String[] args) {
-        String rules = "nothing yet";
-        System.out.println("hello and welcome to go fish");
-        System.out.println("here are the rules" + rules);
-        System.out.println("please enter the number of players");
-        int numberOfPlayer  = Integer.parseInt(in.nextLine());
-        String[] PlayerNames = new String[numberOfPlayer];  
-        System.out.println("please enter the name of the first player");
-        PlayerNames[0] = in.nextLine();
-        for (int i = 1; i < numberOfPlayer; i++) {
-             System.out.println("please enter the name of player "+(i+1));
-            PlayerNames[i] = in.nextLine();
-        }
-         GameMaster game = new GameMaster(numberOfPlayer, PlayerNames);
-        
+    private static Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        System.out.println("Welcome to Go Fish!");
+
+        System.out.print("Enter the number of players: ");
+        int numPlayers = scanner.nextInt();
+        scanner.nextLine(); // Consume newline
+
+        String[] playerNames = new String[numPlayers];
+        for (int i = 0; i < numPlayers; i++) {
+            System.out.print("Enter the name of Player " + (i + 1) + ": ");
+            playerNames[i] = scanner.nextLine();
+        }
+
+        GameMaster gameMaster = new GameMaster(playerNames);
+        gameMaster.startGame();
+
+        while (!gameMaster.isGameOver()) {
+            System.out.println();
+            System.out.println("Current turn: " + gameMaster.getCurrentPlayer().getName());
+            System.out.println("Your hand: " + gameMaster.getCurrentPlayer().getHand());
+            System.out.print("Enter the name of the player you want to ask: ");
+            String playerName = scanner.nextLine();
+            System.out.print("Enter the value you want to ask for: ");
+            String value = scanner.nextLine();
+
+            gameMaster.playTurn(playerName, value);
+            gameMaster.nextTurn();
+        }
+
+        System.out.println();
+        System.out.println("Game over!");
+
+        for (Player player : gameMaster.getPlayers()) {
+            System.out.println(player.getName() + "'s books: " + player.getBooks());
+        }
     }
 }
