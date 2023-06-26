5ef22a08cc31049d049dfa72d3a230ece40f4771
sidanan <134073838+sidanan@users.noreply.github.com>
2023-06-26 16:50:07 -0400

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,38 +1,72 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Player {
+    private String name;
+    private List<Card> hand;
+    private List<Card> books;
 
-	private String name;
-	private ArrayList<Card> hand = new ArrayList<Card>();
-	private int numberOfBooks = 0;
-	private ArrayList<Card> books = new ArrayList<Card>();
+    public Player(String name) {
+        this.name = name;
+        hand = new ArrayList<>();
+        books = new ArrayList<>();
+    }
 
-	Player(String name){
-		this.name = name;
-		hand = GameMaster.takeFromDeck(5);
-	}//end of player
+    public String getName() {
+        return name;
+    }
 
+    public List<Card> getHand() {
+        return hand;
+    }
 
-	public ArrayList<Card> getHand() {
-		return this.hand;
-	}//end of getHand
+    public List<Card> getBooks() {
+        return books;
+    }
 
-	public void setHand(ArrayList<Card> hand) {
-		this.hand = hand;
-	}//end of setHand
+    public void addToHand(Card card) {
+        hand.add(card);
+    }
 
-	public String getName() {
-		return name;
-	}//end of getName
-	public void setName(String name) {
-		this.name = name;
-	}//end of setName
+    public void addToHand(List<Card> cards) {
+        hand.addAll(cards);
+    }
 
-	public int getNumberOfBooks() {
-		return numberOfBooks;
-	}//end of getNumberOfBooks
-	public void setNumberOfBooks(int numberOfBooks) {
-        this.numberOfBooks = numberOfBooks;
-    }//end of setNumberOfBooks
+    public void removeFromHand(List<Card> cards) {
+        hand.removeAll(cards);
+    }
 
+    public boolean isHandEmpty() {
+        return hand.isEmpty();
+    }
+
+    public List<Card> giveCardsOfValue(CardValue value) {
+        List<Card> matchingCards = new ArrayList<>();
+        for (Card card : hand) {
+            if (card.getValue() == value) {
+                matchingCards.add(card);
+            }
+        }
+        hand.removeAll(matchingCards);
+        return matchingCards;
+    }
+
+    public boolean checkForBooks() {
+        for (CardValue value : CardValue.values()) {
+            int count = 0;
+            for (Card card : hand) {
+                if (card.getValue() == value) {
+                    count++;
+                }
+            }
+            if (count == 4) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void addToBooks(List<Card> cards) {
+        books.addAll(cards);
+    }
 }
